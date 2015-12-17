2806542911ec871c590da07a3f1cff6cb9ced40d
Alexandereb <alexeb.dev@gmail.com>
2015-12-17 02:30:51 -0300

    Added new sections in index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,21 +3,26 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   mainInfo: {
    title: 'Refugio',
-   calvaryChapel: 'Calvary Chapel'
+   calvaryChapel: 'Calvary Chapel',
+   link: 'Ver servicio online'
   },
   RegularInfo: {
     title: 'Iglesia Cristiana Calvary Chapel Refugio',
-    description: 'Estudiamos la biblia sistematicamente, versiculo a versiculo, apitulo a capitulo.',
+    description: [
+      { info: 'Te invitamos a nuestras reuniones todos los Domingos a las 10:30 horas en calle Los copihues #5462, comuna de Lo Prado, Santiago.'},
+      { info: 'Estudiamos la biblia sistematicamente, versiculo a versiculo, capitulo a capitulo. De esta manera entendemos completamente la palabra de Dios, en orden cronologico y sin evadir ningun versiculo.'}
+    ],
     links: [
       'algo'
     ]
   },
-  Start: {
+    Start: {
     title: 'Por donde empezar',
       list: [
-        { title: 'Recursos', info: 'Revisa nuestos estudios para descargar y aprende más de la palabra de Dios', link: 'link'},
-        { title: 'Media', info: 'Videos, audios, imagenes y mucho más', link: 'dasdsa'},
-        { title: 'Eventos', info: 'Informate de los futuros eventos', link: 'dsdas'}
+        { title: 'Ministerios', link: 'ministeries'},
+          { title: 'Media', link: 'media'},
+        { title: 'Eventos', link:'news'},
+        { title: 'Pastor', link: 'pastor'}
       ]
   }
 });
